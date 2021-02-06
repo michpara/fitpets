b923c99165258af9f0b0bd3287c54ac9846d215c
Sydney Morrow <smorr126@uottawa.ca>
2021-02-06 17:56:39 -0500

    Added all pet animations

--- app/index.js
+++ app/index.js
@@ -16,6 +16,13 @@ const deviceHeight = device.screen.height;
 //pet animations
 const defaultAnimation = document.getElementById("defaultAnimation");
 const playAnimation = document.getElementById("playAnimation");
+const sleepAnimation = document.getElementById("sleepAnimation");
+const eatAnimation = document.getElementById("eatAnimation");
+const sickAnimation = document.getElementById("sickAnimation");
+
+//helper variables
+const animations = [defaultAnimation, playAnimation, sleepAnimation, eatAnimation, sickAnimation]
+
 
 //happy meter info
 const happyMeter = document.getElementById('happyMeter')
@@ -29,71 +36,92 @@ var hungerWidth = device.screen.width * 0.4;
 const feed = document.getElementById("feed");
 const play = document.getElementById("play");
 
-var penguin = {name: "Skipper", age:0, animation: "penguin_0.png", time:1}
-var otter = {name: "Oscar", age:0, animation: "otter_0.png", time:3}
-var panda = {name: "Bamboo", age:0, animation: "panda_0.png", time:3, time2:1}
-var beaver = {name: "Maple", age:0, animation: "beaver_0.png", time:1, time2:1}
-var dragon = {name: "Drago", age:0, animation: "dragon_0.png", time:1}
-var monkey = {name: "Bonzo", age:0, animation: "monkey_0.png", time:3}
-var turtle = {name: "Shelldon", age:0, animation: "turtle_0.png", time:1}
-var fox = {name: "Fiona", age:0, animation: "fox_0.png", time:3}
-var seal = {name: "Blubby", age:0, animation: "seal_0.png", time:1}
-var bat = {name: "Shade", age:0, animation: "bat_0.png", time:1}
+var penguin = {name: "Skipp", age:0, animation: "penguin_0.png", time:1, time2:1, time3:1, time4:1, time5:3}
+var otter = {name: "Oscar", age:0, animation: "otter_0.png", time:3, time2:3, time3:3, time4:3, time5:3}
+var panda = {name: "Mochi", age:0, animation: "panda_0.png", time:3, time2:1, time3:1, time4:1, time5:3}
+var beaver = {name: "Maple", age:0, animation: "beaver_0.png", time:1, time2:1, time3:1, time4:1, time5:3}
+var dragon = {name: "Drago", age:0, animation: "dragon_0.png", time:1, time2:1, time3:1, time4:1, time5:3}
+var monkey = {name: "Bonzo", age:0, animation: "monkey_0.png", time:3, time2:3, time3:3, time4:3, time5:3}
+var turtle = {name: "Chad", age:0, animation: "turtle_0.png", time:1, time2:1, time3:1, time4:3, time5:3}
+var fox = {name: "Fiona", age:0, animation: "fox_0.png", time:3, time2:6, time3:1, time4:3, time5:3}
+var seal = {name: "Blubb", age:0, animation: "seal_0.png", time:1, time2:3, time3:1, time4:1, time5:3}
+var bat = {name: "Shade", age:0, animation: "bat_0.png", time:1, time2:5, time3:1,  time4:1, time5:3}
 
 
 var possiblePets = [panda,beaver,fox,bat,dragon,turtle,seal,penguin,otter,monkey];
 
-
+//create new pet function
 function createPet(){
+  
+  //for some reason these need to be here for the random to work
   var rand = Math.random()
   var ln = possiblePets.length
   var fl = Math.floor(rand*ln)
  console.log(rand)
  console.log(fl)
  console.log(ln)
+  
   var pet = possiblePets[Math.floor(Math.random() * possiblePets.length)];
   let defaultImage = document.getElementById('defaultImage');
   let playImage = document.getElementById('playImage');
+  let eatImage = document.getElementById('eatImage');
+  let sickImage = document.getElementById('sickImage');
+  let sleepImage = document.getElementById('sleepImage');
+  
   let name = document.getElementById('petName');
   let defaultTime = document.getElementById('anim1');
   let playTime = document.getElementById('anim2');
+  let sickTime = document.getElementById('anim3');
+  let eatTime = document.getElementById('anim4');
+  let sleepTime = document.getElementById('anim5');
+  
   defaultImage.href = "default/" + pet.animation
   playImage.href = "play/play_" + pet.animation
+  sickImage.href = "sick/sick_" + pet.animation
+  eatImage.href = "eat/eat_" + pet.animation
+  sleepImage.href = "sleep/sleep_" + pet.animation
+  
   name.text = pet.name
+  
   defaultTime.to = pet.time
   playTime.to = pet.time2
+  sickTime.to = pet.time3
+  eatTime.to = pet.time4
+  sleepTime.to = pet.time5
 }
 
 //initial set up
 function initialSetUp(){
   
-  defaultAnimation.animate("enable");
-  playAnimation.style.display = "none";
-  
-  hungerMeter.width = hungerWidth;
-  happyMeter.width = happyWidth;
+  let today = new Date()
+  let hour = today.getHours()
+  if(hour >= 23 && hour < 7){
+    switchTo(sleepAnimation)
+  }else{
+    switchTo(defaultAnimation)
+  }
+  var json_object = loadData({"hunger": 0, "happy": 10})
+  hunger = json_object.hunger
+  happy = json_object.happy
+  console.log(hunger)
+  calculateHungerMeter(hunger)
+  calculateHappyMeter(happy)
+  upFoodValue();
 }
 
-//switches the animation to the default one
-function switchToDefault(){
-  playAnimation.animate("disable");
-  
-  playAnimation.style.display = "none";
-  
-  defaultAnimation.style.display ="inline";
-  
-  defaultAnimation.animate("enable")
-}
 
-//switches the animation to the play one
-function switchToPlay(){
-  defaultAnimation.animate("disable")
-  
-  defaultAnimation.style.display ="none";
-  
-  playAnimation.style.display = "inline";
-  
-  playAnimation.animate("enable");
+//switches pet animations
+function switchTo(animation){
+  for(var i =0; i<animations.length; i++){
+    if(animations[i].id != animation.id){
+       animations[i].animate("disable")
+       animations[i].style.display = "none"
+    }else{
+      console.log(animations[i].id)
+      animations[i].style.display = "inline"
+      animations[i].animate("enable")
+    }
+  }
 }
 
 //decrements the happiness meter
@@ -150,16 +178,25 @@ function incrementHunger(){
 
 //switches to play animation on play button click and increments happiness meter
 play.addEventListener("click", (evt) => {
-  switchToPlay()
+  switchTo(playAnimation)
   incrementHappy()
-  setTimeout(switchToDefault, 4000)
+  
+    setTimeout(function() {
+    switchTo(defaultAnimation)
+  }, 4000)
+ 
+  
 })
 
 //switches to feed animation on feed button click and increments hunger meter
 feed.addEventListener("click", (evt) => {
-  //switchToFeed()
+  switchTo(eatAnimation)
   incrementHunger()
-  //setTimeaout(switchToDefault, 4000)
+  
+  setTimeout(function() {
+    switchTo(defaultAnimation)
+  }, 4000)
+ 
 })
 
 //performs the following every hour
